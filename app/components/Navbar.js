b6fc284d2b84e2af3e858e3b19d47bf16f05f305
Kunal-sharma809 <kunnu.8442@gmail.com>
2025-04-10 23:24:34 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
         <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
-                    <img src="/logo.jpg" className="h-8" alt="72MANDC Logo" />
+                    <Image src="/logo.jpg" className="h-8" alt="72MANDC Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">72MANDC</span>
                 </a>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
