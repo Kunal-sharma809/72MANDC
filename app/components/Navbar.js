86de0a0aed746252782401554ee48a877dde044c
Kunal-sharma809 <kunnu.8442@gmail.com>
2025-04-10 23:21:33 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -4,23 +4,45 @@ import Image from 'next/image';
 
 const Navbar = () => {
     return (
-        <div className='flex justify-between items-center bg-[#e5e5e5]'>
-            <nav className="flex justify-between items-center mx-10 p-4 relative w-full fixed top-0">
-                <div className="logo font-bold text-4xl flex gap-2">
-                    <Image src="/logo.jpg" width={54} alt="" />
-                    <span className='font-mono'>72 MANDC</span>
+        <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+                    <img src="/logo.jpg" className="h-8" alt="72MANDC Logo" />
+                    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">72MANDC</span>
+                </a>
+                <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                    </svg>
+                </button>
+                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                        <Link>
+                            <a href="/" className="block py-2 px-3 text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
+                        </Link>
+                        <Link>
+                            <a href="/about" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About Us</a>
+                        </Link>
+                        <Link>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Our Projects</a>
+                        </Link>
+                        <Link>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Sustainability</a>
+                        </Link>
+                        <Link>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">News</a>
+                        </Link>
+                        <Link>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Career</a>
+                        </Link>
+                        <Link>
+                            <a href="#" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact Us</a>
+                        </Link>
+                    </ul>
                 </div>
-                <ul className='flex gap-8'>
-                    <Link href={"/"}><li className='hover:font-semibold hover:underline hover:underline-offset-8 cursor-pointer active:text-[#407050] active:font-semibold'>Home</li></Link>
-                    <Link href={"/about"}><li className='hover:font-semibold hover:underline hover:underline-offset-8 cursor-pointer'>About Us</li></Link>
-                    <Link href={"/projects"}><li className='hover:font-semibold hover:underline hover:underline-offset-8 cursor-pointer'>Our Projects</li></Link>
-                    <Link href={"/sustainability"}><li className='hover:font-semibold hover:underline hover:underline-offset-8 cursor-pointer'>Sustainability</li></Link>
-                    <Link href={"/news"}><li className='hover:font-semibold hover:underline hover:underline-offset-8 cursor-pointer'>News</li></Link>
-                    <Link href={"/career"}><li className='hover:font-semibold hover:underline hover:underline-offset-8 cursor-pointer'>Career</li></Link>
-                    <Link href={"/contact"}><li className='hover:font-semibold hover:underline hover:underline-offset-8 cursor-pointer'>Contact Us</li></Link>
-                </ul>
-            </nav>
-        </div>
+            </div>
+        </nav>
     );
 }
 
