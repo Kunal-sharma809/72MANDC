b21950d2eb08c558e88df516ea1c11f1f3b3d341
Kunal-sharma809 <kunnu.8442@gmail.com>
2025-04-12 23:21:23 +0530

    Update Navbar.js

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -12,8 +12,8 @@ const Navbar = () => {
     }
 
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200">
+            <div className="max-w-screen-xl flex flex-wrap items-center justify-between p-4">
                 <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <Image src="/logo.jpg" className="h-8" alt="72MANDC Logo" />
                     <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">72MANDC</span>
