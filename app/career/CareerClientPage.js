58a0cb5187d8623e386bb0418a3bf2231dcab2ee
Kunal-sharma809 <kunnu.8442@gmail.com>
2025-04-14 13:58:00 +0530

    Update CareerClientPage.js

--- app/career/CareerClientPage.js
+++ app/career/CareerClientPage.js
@@ -107,8 +107,11 @@ const CareerClientPage = () => {
                                 <input className='py-3 px-5 rounded-lg bg-[#ece5cd] w-[48%] outline-none max-lg:w-full' name='user_email' type="email" placeholder='Enter your email' required />
                             </div>
                             <textarea className='py-3 px-5 rounded-lg bg-[#ece5cd] w-full h-32 resize-none outline-none' name="message" id="" placeholder='Enter your message' required></textarea>
-                            <Image src="/career/upload.svg" alt="" />
-                            <label htmlFor="file-upload">Upload Resume: </label>
+                            <div className="flex gap-3">
+                                <Image src="/career/upload.svg" alt="" />
+                                <label htmlFor="file-upload">Upload Resume: </label>
+                            </div>
+                            
                             <div className='bg-[#c7a74a] flex justify-center items-center gap-3 text-white w-full px-5 py-3 rounded-lg cursor-pointer max-md:flex-col'>
                                 
                                 <input type="file" onChange={handleFileChange} />
